7478eb47c8fc4b6141cee62fde9ad59fd453e913
matteoboccardo <91879241+matteoboccardo@users.noreply.github.com>
2021-12-11 19:34:24 +0100

    Update lib.h

--- lib.h
+++ lib.h
@@ -1,1 +1,9 @@
+//
+// Created by bocca on 11/12/2021.
+//
 
+#ifndef INC_8_MAX_ARRAY_MATTEOBOCCARDO_LIB_H
+#define INC_8_MAX_ARRAY_MATTEOBOCCARDO_LIB_H
+
+#endif //INC_8_MAX_ARRAY_MATTEOBOCCARDO_LIB_H
+float massimo(float[],int,float);
