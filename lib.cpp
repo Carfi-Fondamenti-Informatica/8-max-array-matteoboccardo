d9f779f3b83ee03d30f347c94d0471196325ffda
matteoboccardo <91879241+matteoboccardo@users.noreply.github.com>
2021-12-11 19:32:19 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,14 @@
-
+//
+// Created by bocca on 11/12/2021.
+//
+#include "lib.h"
+float massimo (float max[], int n, float m){
+    for(int i=0; i<n; i++){
+        if(i==0){
+            m=max[i];
+        }else if( i>0 & max[i]>m){
+            m=max[i];
+        }
+    }
+    return m;
+}
