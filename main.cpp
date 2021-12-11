e0416453ed0f0c2eb4f9683780e5ab30dd72a6ae
matteoboccardo <91879241+matteoboccardo@users.noreply.github.com>
2021-12-11 19:31:22 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,15 @@
 #include <iostream>
 
 int main(){
+  int n;
+    float m=0;
+    cin >> n;
+    float max[n];
+    for (int i=0;i<n;i++){
+        float a=0;
+        cin >>a;
+        max[i]=a;
+    }
+    cout << massimo (max,n,m);
   return 0;
 }
